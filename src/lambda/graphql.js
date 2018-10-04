2d9a87f84a08885d329dcc075af64c969e40e477
Luchanskiy Oleg <luchansodev@gmail.com>
2018-10-04 16:12:30 +0300

    add some experiments

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -3,6 +3,7 @@ const { ApolloServer, gql } = require("apollo-server-lambda");
 const typeDefs = gql`
   type Query {
     hello: String
+    random: Float
   }
 `;
 
@@ -10,6 +11,9 @@ const resolvers = {
   Query: {
     hello: (root, args, context) => {
       return "Hello, world!";
+    },
+    random: (root, args, context) => {
+      return Math.random();
     }
   }
 };
